fa8994df230f011c84f2744beadafec3f0bb109e
Lisa Jian <lisajian@berkeley.edu>
2019-12-17 17:56:38 -0800

    Added method signatures for interactive solver

---
+++ pkg/interactive_cdcl.py
@@ -0,0 +1,13 @@
+"""
+Interface between pysat code rest of interactive solver.
+"""
+from pkg.pysat import solver
+
+class InteractiveSolver:
+    def __init__():
+
+    def continue_to_conflict():
+
+    def step():
+
+    def current_uip():
